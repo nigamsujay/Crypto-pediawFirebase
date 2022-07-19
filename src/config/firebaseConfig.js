0ef91d72e94fed0e8c62d3ac9664a29620c84255
nigamsujay <80716549+nigamsujay@users.noreply.github.com>
2022-07-19 13:56:44 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDS6FXy9NVra8KebmgUtIOksN7V2-XoICk",
+  authDomain: "cryptopedia-24.firebaseapp.com",
+  projectId: "cryptopedia-24",
+  storageBucket: "cryptopedia-24.appspot.com",
+  messagingSenderId: "583101069875",
+  appId: "1:583101069875:web:aaeb36fda33328ffccc146"
+};
+
+
+export default firebaseConfig;
